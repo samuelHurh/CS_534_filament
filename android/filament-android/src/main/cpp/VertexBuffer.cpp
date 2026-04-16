thetoke/filament

VertexBuffer builder normalized now accepts "false".

android/filament-android/src/main/cpp/VertexBuffer.cpp
@@ -65,9 +65,9 @@ Java_com_google_android_filament_VertexBuffer_nBuilderAttribute(JNIEnv *env, jcl
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_google_android_filament_VertexBuffer_nBuilderNormalized(JNIEnv *env, jclass type,
-        jlong nativeBuilder, jint attribute) {
+        jlong nativeBuilder, jint attribute, jboolean enabled) {
     VertexBuffer::Builder* builder = (VertexBuffer::Builder *) nativeBuilder;
-    builder->normalized((VertexAttribute) attribute);
+    builder->normalized((VertexAttribute) attribute, enabled);
 }
 
 extern "C" JNIEXPORT jlong JNICALL

filament/include/filament/VertexBuffer.h
@@ -64,7 +64,7 @@ public:
                 uint8_t byteStride = 0) noexcept;     // default is attribute size
 
         // no-op if attribute is an invalid enum
-        Builder& normalized(VertexAttribute attribute) noexcept;
+        Builder& normalized(VertexAttribute attribute, bool enabled = true) noexcept;
 
         /**
          * Creates the VertexBuffer object and returns a pointer to it.

filament/src/VertexBuffer.cpp
@@ -100,10 +100,15 @@ VertexBuffer::Builder& VertexBuffer::Builder::attribute(VertexAttribute attribut
     return *this;
 }
 
-VertexBuffer::Builder& VertexBuffer::Builder::normalized(VertexAttribute attribute) noexcept {
+VertexBuffer::Builder& VertexBuffer::Builder::normalized(VertexAttribute attribute,
+        bool enabled) noexcept {
     if (size_t(attribute) < MAX_ATTRIBUTE_BUFFERS_COUNT) {
         AttributeData& entry = mImpl->mAttributes[attribute];
-        entry.flags |= Driver::Attribute::FLAG_NORMALIZED;
+        if (enabled) {
+            entry.flags |= Driver::Attribute::FLAG_NORMALIZED;
+        } else {
+            entry.flags &= ~Driver::Attribute::FLAG_NORMALIZED;
+        }
     }
     return *this;
 }

web/filament-js/filament.d.ts
@@ -87,6 +87,7 @@ export class VertexBuffer$Builder {
                         atype: VertexBuffer$AttributeType,
                         offset: number, stride: number): VertexBuffer$Builder;
     public normalized(attrib: VertexAttribute): VertexBuffer$Builder;
+    public normalizedIf(attrib: VertexAttribute, enabled: boolean): VertexBuffer$Builder;
     public build(engine: Engine): VertexBuffer;
 }
 

web/filament-js/jsbindings.cpp
@@ -745,6 +745,9 @@ class_<VertexBuilder>("VertexBuffer$Builder")
     .BUILDER_FUNCTION("normalized", VertexBuilder, (VertexBuilder* builder,
             VertexAttribute attrib), {
         return &builder->normalized(attrib); })
+    .BUILDER_FUNCTION("normalizedIf", VertexBuilder, (VertexBuilder* builder,
+            VertexAttribute attrib, bool enabled), {
+        return &builder->normalized(attrib, enabled); })
     .BUILDER_FUNCTION("bufferCount", VertexBuilder, (VertexBuilder* builder, int count), {
         return &builder->bufferCount(count); });
 

samuelHurh/CS_534_filament

android/filament-android/src/main/cpp/VertexBuffer.cpp
@@ -65,9 +65,9 @@ Java_com_google_android_filament_VertexBuffer_nBuilderAttribute(JNIEnv *env, jcl
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_google_android_filament_VertexBuffer_nBuilderNormalized(JNIEnv *env, jclass type,
-        jlong nativeBuilder, jint attribute) {
+        jlong nativeBuilder, jint attribute, jboolean normalized) {
     VertexBuffer::Builder* builder = (VertexBuffer::Builder *) nativeBuilder;
-    builder->normalized((VertexAttribute) attribute);
+    builder->normalized((VertexAttribute) attribute, normalized);
 }
 
 extern "C" JNIEXPORT jlong JNICALL

filament/include/filament/VertexBuffer.h
@@ -64,7 +64,7 @@ public:
                 uint8_t byteStride = 0) noexcept;     // default is attribute size
 
         // no-op if attribute is an invalid enum
-        Builder& normalized(VertexAttribute attribute) noexcept;
+        Builder& normalized(VertexAttribute attribute, bool normalize = true) noexcept;
 
         /**
          * Creates the VertexBuffer object and returns a pointer to it.

filament/src/VertexBuffer.cpp
@@ -100,10 +100,15 @@ VertexBuffer::Builder& VertexBuffer::Builder::attribute(VertexAttribute attribut
     return *this;
 }
 
-VertexBuffer::Builder& VertexBuffer::Builder::normalized(VertexAttribute attribute) noexcept {
+VertexBuffer::Builder& VertexBuffer::Builder::normalized(VertexAttribute attribute,
+        bool normalized) noexcept {
     if (size_t(attribute) < MAX_ATTRIBUTE_BUFFERS_COUNT) {
         AttributeData& entry = mImpl->mAttributes[attribute];
-        entry.flags |= Driver::Attribute::FLAG_NORMALIZED;
+        if (normalized) {
+            entry.flags |= Driver::Attribute::FLAG_NORMALIZED;
+        } else {
+            entry.flags &= ~Driver::Attribute::FLAG_NORMALIZED;
+        }
     }
     return *this;
 }

web/filament-js/filament.d.ts
@@ -87,6 +87,7 @@ export class VertexBuffer$Builder {
                         atype: VertexBuffer$AttributeType,
                         offset: number, stride: number): VertexBuffer$Builder;
     public normalized(attrib: VertexAttribute): VertexBuffer$Builder;
+    public normalizedIf(attrib: VertexAttribute, normalized: boolean): VertexBuffer$Builder;
     public build(engine: Engine): VertexBuffer;
 }
 

web/filament-js/jsbindings.cpp
@@ -745,6 +745,9 @@ class_<VertexBuilder>("VertexBuffer$Builder")
     .BUILDER_FUNCTION("normalized", VertexBuilder, (VertexBuilder* builder,
             VertexAttribute attrib), {
         return &builder->normalized(attrib); })
+    .BUILDER_FUNCTION("normalizedIf", VertexBuilder, (VertexBuilder* builder,
+            VertexAttribute attrib, bool normalized), {
+        return &builder->normalized(attrib, normalized); })
     .BUILDER_FUNCTION("bufferCount", VertexBuilder, (VertexBuilder* builder, int count), {
         return &builder->bufferCount(count); });
 