jiapei-nexera/filament

matc: add --template option.
This makes it easier to generate ubershaders without the fancy CMake
machinery available in its `configure_file` command.

tools/matc/src/matc/CommandlineConfig.cpp
@@ -59,6 +59,10 @@ static void usage(char* name) {
             "       Add a preprocessor define macro via <macro>=<value>. <value> defaults to 1 if omitted.\n"
             "       Can be repeated to specify multiple definitions:\n"
             "           MATC -Dfoo=1 -Dbar -Dbuzz=100 ...\n\n"
+            "   --template <macro>=<string>, -T<macro>=<string>\n"
+            "       Replaces ${MACRO} with specified string before parsing\n"
+            "       Can be repeated to specify multiple macros:\n"
+            "           MATC -TBLENDING=fade -TDOUBLESIDED=false ...\n\n"
             "   --reflect, -r\n"
             "       Reflect the specified metadata as JSON: parameters\n\n"
             "   --variant-filter=<filter>, -V <filter>\n"
@@ -128,8 +132,7 @@ CommandlineConfig::CommandlineConfig(int argc, char** argv) : Config(), mArgc(ar
     mIsValid = parse();
 }
 
-static void parseDefine(std::string defineString,
-        std::unordered_map<std::string, std::string>& defines) {
+static void parseDefine(std::string defineString, Config::StringViewMap& defines) {
     const char* const defineArg = defineString.c_str();
     const size_t length = defineString.length();
 
@@ -156,7 +159,7 @@ static void parseDefine(std::string defineString,
 }
 
 bool CommandlineConfig::parse() {
-    static constexpr const char* OPTSTR = "hlxo:f:dm:a:p:D:OSEr:vV:gtw";
+    static constexpr const char* OPTSTR = "hlxo:f:dm:a:p:D:T:OSEr:vV:gtw";
     static const struct option OPTIONS[] = {
             { "help",                    no_argument, nullptr, 'h' },
             { "license",                 no_argument, nullptr, 'l' },
@@ -172,6 +175,7 @@ bool CommandlineConfig::parse() {
             { "preprocessor-only",       no_argument, nullptr, 'E' },
             { "api",               required_argument, nullptr, 'a' },
             { "define",            required_argument, nullptr, 'D' },
+            { "template",          required_argument, nullptr, 'T' },
             { "reflect",           required_argument, nullptr, 'r' },
             { "print",                   no_argument, nullptr, 't' },
             { "version",                 no_argument, nullptr, 'v' },
@@ -241,6 +245,9 @@ bool CommandlineConfig::parse() {
                 break;
             case 'D':
                 parseDefine(arg, mDefines);
+                break;
+            case 'T':
+                parseDefine(arg, mTemplateMap);
                 break;
             case 'v':
                 // Similar to --help, the --version command does an early exit in order to avoid

samuelHurh/CS_534_filament

tools/matc/src/matc/CommandlineConfig.cpp
@@ -59,6 +59,11 @@ static void usage(char* name) {
             "       Add a preprocessor define macro via <macro>=<value>. <value> defaults to 1 if omitted.\n"
             "       Can be repeated to specify multiple definitions:\n"
             "           MATC -Dfoo=1 -Dbar -Dbuzz=100 ...\n\n"
+            "   --template <macro>=<string>, -T<macro>=<string>\n"
+            "       Replaces ${MACRO} with specified string before parsing\n"
+            "       Unlike --define, this applies to the material specification, not GLSL.\n"
+            "       Can be repeated to specify multiple macros:\n"
+            "           MATC -TBLENDING=fade -TDOUBLESIDED=false ...\n\n"
             "   --reflect, -r\n"
             "       Reflect the specified metadata as JSON: parameters\n\n"
             "   --variant-filter=<filter>, -V <filter>\n"
@@ -128,8 +133,7 @@ CommandlineConfig::CommandlineConfig(int argc, char** argv) : Config(), mArgc(ar
     mIsValid = parse();
 }
 
-static void parseDefine(std::string defineString,
-        std::unordered_map<std::string, std::string>& defines) {
+static void parseDefine(std::string defineString, Config::StringReplacementMap& defines) {
     const char* const defineArg = defineString.c_str();
     const size_t length = defineString.length();
 
@@ -156,7 +160,7 @@ static void parseDefine(std::string defineString,
 }
 
 bool CommandlineConfig::parse() {
-    static constexpr const char* OPTSTR = "hlxo:f:dm:a:p:D:OSEr:vV:gtw";
+    static constexpr const char* OPTSTR = "hlxo:f:dm:a:p:D:T:OSEr:vV:gtw";
     static const struct option OPTIONS[] = {
             { "help",                    no_argument, nullptr, 'h' },
             { "license",                 no_argument, nullptr, 'l' },
@@ -172,6 +176,7 @@ bool CommandlineConfig::parse() {
             { "preprocessor-only",       no_argument, nullptr, 'E' },
             { "api",               required_argument, nullptr, 'a' },
             { "define",            required_argument, nullptr, 'D' },
+            { "template",          required_argument, nullptr, 'T' },
             { "reflect",           required_argument, nullptr, 'r' },
             { "print",                   no_argument, nullptr, 't' },
             { "version",                 no_argument, nullptr, 'v' },
@@ -241,6 +246,9 @@ bool CommandlineConfig::parse() {
                 break;
             case 'D':
                 parseDefine(arg, mDefines);
+                break;
+            case 'T':
+                parseDefine(arg, mTemplateMap);
                 break;
             case 'v':
                 // Similar to --help, the --version command does an early exit in order to avoid