milieu-us/filament

Add bitcast to utils.

libs/utils/include/utils/algorithm.h
@@ -255,6 +255,18 @@ RandomAccessIterator partition_point(
     return first;
 }
 
+template <class To, class From>
+typename std::enable_if<
+    (sizeof(To) == sizeof(From)) &&
+    std::is_trivially_copyable<From>::value &&
+    std::is_trivial<To>::value,
+    // this implementation requires that To is trivially default constructible
+    To>::type
+// constexpr support needs compiler magic
+bit_cast(const From &src) noexcept {
+    return reinterpret_cast<const To&>(src);
+}
+
 } // namespace utils
 
 #endif // TNT_UTILS_ALGORITHM_H

samuelHurh/CS_534_filament

libs/utils/include/utils/algorithm.h
@@ -255,6 +255,18 @@ RandomAccessIterator partition_point(
     return first;
 }
 
+template <class To, class From>
+typename std::enable_if_t<
+    (sizeof(To) == sizeof(From)) &&
+    std::is_trivially_copyable<From>::value &&
+    std::is_trivial<To>::value,
+    // this implementation requires that To is trivially default constructible
+    To>
+// constexpr support needs compiler magic
+bit_cast(const From &src) noexcept {
+    return reinterpret_cast<const To&>(src);
+}
+
 } // namespace utils
 
 #endif // TNT_UTILS_ALGORITHM_H