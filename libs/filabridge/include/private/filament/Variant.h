huangjingpei/filament

Rework material Variant filters
Variants are no longer just a bit mask, but rather a combination of
some bits, depending on the variant. Because of that the variant filer
must be updated.

Basically we now make a distinction between the "variants" as a public
material API and the actual `Variant` data type.

This change does the impedance match between the two.

libs/filabridge/include/private/filament/Variant.h
@@ -20,6 +20,8 @@
 #include <stdint.h>
 #include <cstddef>
 
+#include <filament/MaterialEnums.h>
+
 #include <utils/bitset.h>
 
 namespace filament {
@@ -152,6 +154,10 @@ struct Variant {
         return !isValidStandardVariant(variant) && !isValidDepthVariant(variant);
     }
 
+    static constexpr bool isValid(Variant variant) noexcept {
+        return isValidStandardVariant(variant) || isValidDepthVariant(variant);
+    }
+
     static constexpr Variant filterVariantVertex(Variant variant) noexcept {
         // filter out vertex variants that are not needed. For e.g. fog doesn't affect the
         // vertex shader.
@@ -206,85 +212,15 @@ struct Variant {
         return Variant(key & rhs);
     }
 
+    static Variant filterVariant(
+            Variant variant, UserVariantFilterMask filterMask) noexcept;
+
 private:
     void set(bool v, type_t mask) noexcept {
         key = (key & ~mask) | (v ? mask : type_t(0));
     }
 };
 
-namespace details {
-
-// compile time sanity-check tests
-
-constexpr inline bool reserved_is_not_valid() noexcept {
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        bool is_valid = Variant::isValidDepthVariant(variant) ||
-                Variant::isValidStandardVariant(variant);
-        bool is_reserved = Variant::isReserved(variant);
-        if (is_valid == is_reserved) {
-            return false;
-        }
-    }
-    return true;
-}
-
-constexpr inline size_t reserved_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isReserved(variant)) { count++; }
-    }
-    return count;
-}
-
-constexpr inline size_t valid_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isValidDepthVariant(variant) ||
-            Variant::isValidStandardVariant(variant)) {
-            count++;
-        }
-    }
-    return count;
-}
-
-constexpr inline size_t vertex_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isValidDepthVariant(variant) ||
-            Variant::isValidStandardVariant(variant)) {
-            if (Variant::isVertexVariant(variant)) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
-constexpr inline size_t fragment_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isValidDepthVariant(variant) ||
-            Variant::isValidStandardVariant(variant)) {
-            if (Variant::filterVariantFragment(variant).key == i) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
-static_assert(reserved_is_not_valid());
-static_assert(reserved_variant_count() == 82);
-static_assert(valid_variant_count()    == 46);
-static_assert(vertex_variant_count()   == 16 - (2 + 0) + 4 - 0);    // 18
-static_assert(fragment_variant_count() == 32 - (4 + 8) + 4 - 1);    // 25
-
-} // namespace details
 } // namespace filament
 
 #endif // TNT_FILABRIDGE_VARIANT_H

libs/filabridge/src/Variant.cpp
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <private/filament/Variant.h>
+
+namespace filament {
+
+Variant Variant::filterVariant(
+        Variant variant, UserVariantFilterMask filterMask) noexcept {
+    // these are easy to filter by just removing the corresponding bit
+    if (filterMask & (uint32_t)UserVariantFilterBit::DIRECTIONAL_LIGHTING) {
+        variant.key &= ~(filterMask & DIR);
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::DYNAMIC_LIGHTING) {
+        variant.key &= ~(filterMask & DYN);
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::SKINNING) {
+        variant.key &= ~(filterMask & SKN);
+    }
+    if (!isValidDepthVariant(variant)) {
+        // we can't remove FOG from depth variants, this would, in fact, remove picking
+        if (filterMask & (uint32_t)UserVariantFilterBit::FOG) {
+            variant.key &= ~(filterMask & FOG);
+        }
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::SHADOW_RECEIVER) {
+        variant.key &= ~(filterMask & SRE);
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::VSM) {
+        variant.key &= ~(filterMask & VSM);
+    }
+    return variant;
+}
+
+
+
+namespace details {
+
+// compile time sanity-check tests
+
+constexpr inline bool reserved_is_not_valid() noexcept {
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        bool is_valid = Variant::isValid(variant);
+        bool is_reserved = Variant::isReserved(variant);
+        if (is_valid == is_reserved) {
+            return false;
+        }
+    }
+    return true;
+}
+
+constexpr inline size_t reserved_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isReserved(variant)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+constexpr inline size_t valid_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isValid(variant)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+constexpr inline size_t vertex_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isValid(variant)) {
+            if (Variant::isVertexVariant(variant)) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+constexpr inline size_t fragment_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isValid(variant)) {
+            if (Variant::isFragmentVariant(variant)) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+static_assert(reserved_is_not_valid());
+static_assert(reserved_variant_count() == 82);
+static_assert(valid_variant_count()    == 46);
+static_assert(vertex_variant_count()   == 16 - (2 + 0) + 4 - 0);    // 18
+static_assert(fragment_variant_count() == 32 - (4 + 8) + 4 - 1);    // 25
+
+} // namespace details
+
+} // namespace filament

libs/filamat/src/MaterialVariants.cpp
@@ -21,24 +21,26 @@
 namespace filamat {
 
 std::vector<Variant> determineSurfaceVariants(
-        filament::Variant::type_t variantFilter, bool isLit, bool shadowMultiplier) {
+        filament::UserVariantFilterMask userVariantFilter, bool isLit, bool shadowMultiplier) {
     std::vector<Variant> variants;
-    filament::Variant::type_t variantMask = ~variantFilter;
     for (filament::Variant::type_t k = 0; k < filament::VARIANT_COUNT; k++) {
         filament::Variant variant(k);
         if (filament::Variant::isReserved(variant)) {
             continue;
         }
 
-        // Remove variants for unlit materials
-        filament::Variant v = filament::Variant::filterVariant(
-                variant & variantMask, isLit || shadowMultiplier);
+        filament::Variant filteredVariant =
+                filament::Variant::filterVariant(variant, userVariantFilter);
 
-        if (filament::Variant::filterVariantVertex(v) == variant) {
+        // Remove variants for unlit materials
+        filteredVariant = filament::Variant::filterVariant(
+                filteredVariant, isLit || shadowMultiplier);
+
+        if (filament::Variant::filterVariantVertex(filteredVariant) == variant) {
             variants.emplace_back(variant, filament::backend::ShaderType::VERTEX);
         }
 
-        if (filament::Variant::filterVariantFragment(v) == variant) {
+        if (filament::Variant::filterVariantFragment(filteredVariant) == variant) {
             variants.emplace_back(variant, filament::backend::ShaderType::FRAGMENT);
         }
     }

samuelHurh/CS_534_filament

libs/filabridge/include/private/filament/Variant.h
@@ -20,6 +20,8 @@
 #include <stdint.h>
 #include <cstddef>
 
+#include <filament/MaterialEnums.h>
+
 #include <utils/bitset.h>
 
 namespace filament {
@@ -152,6 +154,10 @@ struct Variant {
         return !isValidStandardVariant(variant) && !isValidDepthVariant(variant);
     }
 
+    static constexpr bool isValid(Variant variant) noexcept {
+        return isValidStandardVariant(variant) || isValidDepthVariant(variant);
+    }
+
     static constexpr Variant filterVariantVertex(Variant variant) noexcept {
         // filter out vertex variants that are not needed. For e.g. fog doesn't affect the
         // vertex shader.
@@ -206,85 +212,15 @@ struct Variant {
         return Variant(key & rhs);
     }
 
+    static Variant filterUserVariant(
+            Variant variant, UserVariantFilterMask filterMask) noexcept;
+
 private:
     void set(bool v, type_t mask) noexcept {
         key = (key & ~mask) | (v ? mask : type_t(0));
     }
 };
 
-namespace details {
-
-// compile time sanity-check tests
-
-constexpr inline bool reserved_is_not_valid() noexcept {
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        bool is_valid = Variant::isValidDepthVariant(variant) ||
-                Variant::isValidStandardVariant(variant);
-        bool is_reserved = Variant::isReserved(variant);
-        if (is_valid == is_reserved) {
-            return false;
-        }
-    }
-    return true;
-}
-
-constexpr inline size_t reserved_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isReserved(variant)) { count++; }
-    }
-    return count;
-}
-
-constexpr inline size_t valid_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isValidDepthVariant(variant) ||
-            Variant::isValidStandardVariant(variant)) {
-            count++;
-        }
-    }
-    return count;
-}
-
-constexpr inline size_t vertex_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isValidDepthVariant(variant) ||
-            Variant::isValidStandardVariant(variant)) {
-            if (Variant::isVertexVariant(variant)) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
-constexpr inline size_t fragment_variant_count() noexcept {
-    size_t count = 0;
-    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
-        const Variant variant(i);
-        if (Variant::isValidDepthVariant(variant) ||
-            Variant::isValidStandardVariant(variant)) {
-            if (Variant::filterVariantFragment(variant).key == i) {
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
-static_assert(reserved_is_not_valid());
-static_assert(reserved_variant_count() == 82);
-static_assert(valid_variant_count()    == 46);
-static_assert(vertex_variant_count()   == 16 - (2 + 0) + 4 - 0);    // 18
-static_assert(fragment_variant_count() == 32 - (4 + 8) + 4 - 1);    // 25
-
-} // namespace details
 } // namespace filament
 
 #endif // TNT_FILABRIDGE_VARIANT_H

libs/filabridge/src/Variant.cpp
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <private/filament/Variant.h>
+
+namespace filament {
+
+Variant Variant::filterUserVariant(
+        Variant variant, UserVariantFilterMask filterMask) noexcept {
+    // these are easy to filter by just removing the corresponding bit
+    if (filterMask & (uint32_t)UserVariantFilterBit::DIRECTIONAL_LIGHTING) {
+        variant.key &= ~(filterMask & DIR);
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::DYNAMIC_LIGHTING) {
+        variant.key &= ~(filterMask & DYN);
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::SKINNING) {
+        variant.key &= ~(filterMask & SKN);
+    }
+    if (!isValidDepthVariant(variant)) {
+        // we can't remove FOG from depth variants, this would, in fact, remove picking
+        if (filterMask & (uint32_t)UserVariantFilterBit::FOG) {
+            variant.key &= ~(filterMask & FOG);
+        }
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::SHADOW_RECEIVER) {
+        variant.key &= ~(filterMask & SRE);
+    }
+    if (filterMask & (uint32_t)UserVariantFilterBit::VSM) {
+        variant.key &= ~(filterMask & VSM);
+    }
+    return variant;
+}
+
+
+
+namespace details {
+
+// compile time sanity-check tests
+
+constexpr inline bool reserved_is_not_valid() noexcept {
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        bool is_valid = Variant::isValid(variant);
+        bool is_reserved = Variant::isReserved(variant);
+        if (is_valid == is_reserved) {
+            return false;
+        }
+    }
+    return true;
+}
+
+constexpr inline size_t reserved_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isReserved(variant)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+constexpr inline size_t valid_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isValid(variant)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+constexpr inline size_t vertex_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isValid(variant)) {
+            if (Variant::isVertexVariant(variant)) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+constexpr inline size_t fragment_variant_count() noexcept {
+    size_t count = 0;
+    for (Variant::type_t i = 0; i < VARIANT_COUNT; i++) {
+        const Variant variant(i);
+        if (Variant::isValid(variant)) {
+            if (Variant::isFragmentVariant(variant)) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+static_assert(reserved_is_not_valid());
+static_assert(reserved_variant_count() == 82);
+static_assert(valid_variant_count()    == 46);
+static_assert(vertex_variant_count()   == 16 - (2 + 0) + 4 - 0);    // 18
+static_assert(fragment_variant_count() == 32 - (4 + 8) + 4 - 1);    // 25
+
+} // namespace details
+
+} // namespace filament

libs/filamat/src/MaterialVariants.cpp
@@ -21,24 +21,26 @@
 namespace filamat {
 
 std::vector<Variant> determineSurfaceVariants(
-        filament::Variant::type_t variantFilter, bool isLit, bool shadowMultiplier) {
+        filament::UserVariantFilterMask userVariantFilter, bool isLit, bool shadowMultiplier) {
     std::vector<Variant> variants;
-    filament::Variant::type_t variantMask = ~variantFilter;
     for (filament::Variant::type_t k = 0; k < filament::VARIANT_COUNT; k++) {
         filament::Variant variant(k);
         if (filament::Variant::isReserved(variant)) {
             continue;
         }
 
-        // Remove variants for unlit materials
-        filament::Variant v = filament::Variant::filterVariant(
-                variant & variantMask, isLit || shadowMultiplier);
+        filament::Variant filteredVariant =
+                filament::Variant::filterUserVariant(variant, userVariantFilter);
 
-        if (filament::Variant::filterVariantVertex(v) == variant) {
+        // Remove variants for unlit materials
+        filteredVariant = filament::Variant::filterVariant(
+                filteredVariant, isLit || shadowMultiplier);
+
+        if (filament::Variant::filterVariantVertex(filteredVariant) == variant) {
             variants.emplace_back(variant, filament::backend::ShaderType::VERTEX);
         }
 
-        if (filament::Variant::filterVariantFragment(v) == variant) {
+        if (filament::Variant::filterVariantFragment(filteredVariant) == variant) {
             variants.emplace_back(variant, filament::backend::ShaderType::FRAGMENT);
         }
     }