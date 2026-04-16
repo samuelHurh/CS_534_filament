Oo121oO/filament

Add simple subpass support to the Driver API.
This makes two changes to the Driver API:
1. Adds the beginSubpass() method.
2. Adds subpassMask to RenderPassParams.

Locally I've proven that this PR is sufficient by making a series of
Vulkan changes; sneak peek is available at commit
bfef6914a7d288bbd25926c02ec93d231a8c0d07.

Note that this PR moves the new usage flag to a different texture;
I had placed it on the wrong one.

See also #2780.

filament/backend/src/metal/MetalDriver.mm
@@ -758,6 +758,8 @@ void MetalDriver::beginRenderPass(Handle<HwRenderTarget> rth,
     mContext->cullModeState.invalidate();
 }
 
+void MetalDriver::beginSubpass(int dummy) {}
+
 void MetalDriver::endRenderPass(int dummy) {
     [mContext->currentRenderPassEncoder endEncoding];
 

filament/backend/src/noop/NoopDriver.cpp
@@ -222,6 +222,9 @@ void NoopDriver::beginRenderPass(Handle<HwRenderTarget> rth, const RenderPassPar
 void NoopDriver::endRenderPass(int) {
 }
 
+void NoopDriver::beginSubpass(int) {
+}
+
 void NoopDriver::setRenderPrimitiveBuffer(Handle<HwRenderPrimitive> rph,
         Handle<HwVertexBuffer> vbh, Handle<HwIndexBuffer> ibh,
         uint32_t enabledAttributes) {

filament/backend/src/opengl/OpenGLDriver.cpp
@@ -2192,6 +2192,9 @@ void OpenGLDriver::endRenderPass(int) {
 }
 
 
+void OpenGLDriver::beginSubpass(int) {}
+
+
 void OpenGLDriver::resolvePass(ResolveAction action, GLRenderTarget const* rt,
         backend::TargetBufferFlags discardFlags) noexcept {
     assert(rt->gl.fbo_read);

filament/backend/src/vulkan/VulkanDriver.cpp
@@ -992,6 +992,9 @@ void VulkanDriver::endRenderPass(int) {
     mContext.currentRenderPass.renderPass = VK_NULL_HANDLE;
 }
 
+void VulkanDriver::beginSubpass(int) {
+}
+
 void VulkanDriver::setRenderPrimitiveBuffer(Handle<HwRenderPrimitive> rph,
         Handle<HwVertexBuffer> vbh, Handle<HwIndexBuffer> ibh,
         uint32_t enabledAttributes) {

filament/src/Renderer.cpp
@@ -381,6 +381,9 @@ void FRenderer::renderJob(ArenaScope& arena, FView& view) {
             .height = config.svp.height,
             .format = config.hdrFormat
     };
+    if (colorGradingConfig.asSubpass) {
+        desc.usage |= backend::TextureUsage::SUBPASS_INPUT;
+    }
     colorPass(fg, "Color Pass", desc, config, colorGradingConfig, pass, view);
 
     // TODO: look for refraction draw calls only if screen-space refraction is enabled
@@ -654,8 +657,7 @@ FrameGraphId<FrameGraphTexture> FRenderer::colorPass(FrameGraph& fg, const char*
                     data.output = builder.createTexture("Tonemapped Buffer", {
                             .width = colorBufferDesc.width,
                             .height = colorBufferDesc.height,
-                            .format = colorGradingConfig.ldrFormat,
-                            .usage = backend::TextureUsage::SUBPASS_INPUT
+                            .format = colorGradingConfig.ldrFormat
                     });
                     data.output = builder.write(data.output);
                 }
@@ -695,11 +697,15 @@ FrameGraphId<FrameGraphTexture> FRenderer::colorPass(FrameGraph& fg, const char*
 
                 out.params.clearColor = data.clearColor;
 
-                driver.beginRenderPass(out.target, out.params);
-                pass.executeCommands(resources.getPassName());
-
                 if (colorGradingConfig.asSubpass) {
-                    // post-processing....
+                    // TODO: update params for the subpass material here (before beginRenderPass)
+                    // because uploading uniforms from within a render pass is illegal in Vulkan.
+                    // See #2780.
+                    out.params.subpassMask = 1;
+                    driver.beginRenderPass(out.target, out.params);
+                    pass.executeCommands(resources.getPassName());
+
+                    driver.beginSubpass();
                     ppm.colorGradingSubpass(driver,
                             view.getColorGrading(),
                             view.getVignetteOptions(),
@@ -708,6 +714,9 @@ FrameGraphId<FrameGraphTexture> FRenderer::colorPass(FrameGraph& fg, const char*
                             colorGradingConfig.dithering,
                             out.params.viewport.width,
                             out.params.viewport.height);
+                } else {
+                    driver.beginRenderPass(out.target, out.params);
+                    pass.executeCommands(resources.getPassName());
                 }
 
                 driver.endRenderPass();

samuelHurh/CS_534_filament

filament/backend/src/metal/MetalDriver.mm
@@ -758,6 +758,8 @@ void MetalDriver::beginRenderPass(Handle<HwRenderTarget> rth,
     mContext->cullModeState.invalidate();
 }
 
+void MetalDriver::nextSubpass(int dummy) {}
+
 void MetalDriver::endRenderPass(int dummy) {
     [mContext->currentRenderPassEncoder endEncoding];
 

filament/backend/src/noop/NoopDriver.cpp
@@ -222,6 +222,9 @@ void NoopDriver::beginRenderPass(Handle<HwRenderTarget> rth, const RenderPassPar
 void NoopDriver::endRenderPass(int) {
 }
 
+void NoopDriver::nextSubpass(int) {
+}
+
 void NoopDriver::setRenderPrimitiveBuffer(Handle<HwRenderPrimitive> rph,
         Handle<HwVertexBuffer> vbh, Handle<HwIndexBuffer> ibh,
         uint32_t enabledAttributes) {

filament/backend/src/opengl/OpenGLDriver.cpp
@@ -2192,6 +2192,9 @@ void OpenGLDriver::endRenderPass(int) {
 }
 
 
+void OpenGLDriver::nextSubpass(int) {}
+
+
 void OpenGLDriver::resolvePass(ResolveAction action, GLRenderTarget const* rt,
         backend::TargetBufferFlags discardFlags) noexcept {
     assert(rt->gl.fbo_read);

filament/backend/src/vulkan/VulkanDriver.cpp
@@ -992,6 +992,9 @@ void VulkanDriver::endRenderPass(int) {
     mContext.currentRenderPass.renderPass = VK_NULL_HANDLE;
 }
 
+void VulkanDriver::nextSubpass(int) {
+}
+
 void VulkanDriver::setRenderPrimitiveBuffer(Handle<HwRenderPrimitive> rph,
         Handle<HwVertexBuffer> vbh, Handle<HwIndexBuffer> ibh,
         uint32_t enabledAttributes) {

filament/src/Renderer.cpp
@@ -381,6 +381,9 @@ void FRenderer::renderJob(ArenaScope& arena, FView& view) {
             .height = config.svp.height,
             .format = config.hdrFormat
     };
+    if (colorGradingConfig.asSubpass) {
+        desc.usage |= backend::TextureUsage::SUBPASS_INPUT;
+    }
     colorPass(fg, "Color Pass", desc, config, colorGradingConfig, pass, view);
 
     // TODO: look for refraction draw calls only if screen-space refraction is enabled
@@ -654,8 +657,7 @@ FrameGraphId<FrameGraphTexture> FRenderer::colorPass(FrameGraph& fg, const char*
                     data.output = builder.createTexture("Tonemapped Buffer", {
                             .width = colorBufferDesc.width,
                             .height = colorBufferDesc.height,
-                            .format = colorGradingConfig.ldrFormat,
-                            .usage = backend::TextureUsage::SUBPASS_INPUT
+                            .format = colorGradingConfig.ldrFormat
                     });
                     data.output = builder.write(data.output);
                 }
@@ -695,11 +697,15 @@ FrameGraphId<FrameGraphTexture> FRenderer::colorPass(FrameGraph& fg, const char*
 
                 out.params.clearColor = data.clearColor;
 
-                driver.beginRenderPass(out.target, out.params);
-                pass.executeCommands(resources.getPassName());
-
                 if (colorGradingConfig.asSubpass) {
-                    // post-processing....
+                    // TODO: commit uniforms for the subpass material here (before beginRenderPass)
+                    // because uploading uniforms from within a render pass is illegal in Vulkan.
+                    // See #2780.
+                    out.params.subpassMask = 1;
+                    driver.beginRenderPass(out.target, out.params);
+                    pass.executeCommands(resources.getPassName());
+
+                    driver.nextSubpass();
                     ppm.colorGradingSubpass(driver,
                             view.getColorGrading(),
                             view.getVignetteOptions(),
@@ -708,6 +714,9 @@ FrameGraphId<FrameGraphTexture> FRenderer::colorPass(FrameGraph& fg, const char*
                             colorGradingConfig.dithering,
                             out.params.viewport.width,
                             out.params.viewport.height);
+                } else {
+                    driver.beginRenderPass(out.target, out.params);
+                    pass.executeCommands(resources.getPassName());
                 }
 
                 driver.endRenderPass();